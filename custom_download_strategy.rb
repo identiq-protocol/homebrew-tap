9ad77d7540e14f1240a00307e35a57611ada79f6
roytev <roytev@gmail.com>
2023-07-04 20:19:50 +0300

    Update custom_download_strategy.rb

--- custom_download_strategy.rb
+++ custom_download_strategy.rb
@@ -2,36 +2,7 @@ require "download_strategy"
 
 class JfrogDownloadStrategy < CurlDownloadStrategy
   def initialize(url, name, version, **meta)
+    meta[:user] = ENV["HOMEBREW_JFROG_SECRET"]
     super
-    @auth_token = ENV["HOMEBREW_JFROG_SECRET"]
-  end
-
-  def curl_args
-    args = super
-    args << "-u" << ":#{@auth_token}" if @auth_token
-    args
-  end
-end
-
-class DownloadStrategyDetector
-  class << self
-    module Compat
-      def detect(url, using = nil)
-        strategy = super
-
-        strategy
-      end
-
-      def detect_from_symbol(symbol)
-        case symbol
-        when :jfrog
-          JfrogDownloadStrategy
-        else
-          super(symbol)
-        end
-      end
-    end
-
-    prepend Compat
   end
 end
